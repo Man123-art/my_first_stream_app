d7a2edf313a0a57f2c1a284c3cf6b81f2843432a
Man123-art <pmangesh547@gmail.com>
2025-02-18 13:50:56 +0530

    Update analysis.py

--- pages/analysis.py
+++ pages/analysis.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
-
 st.set_page_config(
    page_title='Multipage App',
    page_icon='❤️')
-st.table(df)
 st.title('Main Page')
 df=pd.read_csv('Automobile.csv')
+st.table(df)
 st.scatter_chart(df,x='length',y='mileage',color='company')
